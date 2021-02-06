16214249d87be95e98f959ccea148deb1f1309c1
jacquesme <jacquesvandermerwe1974@gmail.com>
2021-02-06 13:53:54 +0200

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container bg-blue-900">
+    <h1 class="text-white border-2">Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
