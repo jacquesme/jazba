d38dd25906aba60ba29775c418f66dd673362e56
jacquesme <jacquesvandermerwe1974@gmail.com>
2021-02-15 13:40:31 +0200

    about page completed

---
+++ pages/about.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="px-10 md:px-40 pb-16">
+    <div class="mt-14">
+      <p class="text-center text-xs">
+        If you’ve ever had to sacrifice and persevere then you understand what
+        this brand is all about. There is a passion that is often driven by the
+        sense of self fulfilment and Purpose, a purpose that is fueled by true
+        desire and an understanding of where you’re coming from and where you
+        aim to be. Tredwear is a brand that is inspired by true life experiences
+        of the founder and also the lives of each hardworking person or
+        dedicated sportspeople, who have experienced the true feeling of
+        immeasurable fulfillment that is not essentially driven by results , but
+        by dedication and self-belief.
+      </p>
+      <p class="text-center text-xs mt-6">
+        Treadwear is a feeling, a passion, a push, a determination, a will, a
+        character, a belief, a feeling, an attitude and the courage to persevere
+        and not give up. We at Treadwear have conveniently found a way to
+        balance quality, price and affordability of all our products, you don’t
+        need to break the bank to be the best. We create superior products that
+        are true to an active lifestyle. Our manufacturing processes have been
+        centered on the ergonomic form, comfort, support, stability, agility and
+        aesthetics mixed with a complete technical understanding of various
+        footwear needs to deliver an enhanced performance or comfortable
+        lifestyle.
+      </p>
+      <p class="text-center text-xs">
+        Only TREADWEAR dares to be original, meaningful and Passionate. Join Us
+        Today.
+      </p>
+      <div class="w-36 h-auto m-auto mt-14">
+        <img src="/img/milana.png" alt="milana" class="" />
+        <h3 class="text-center text-xs font-bold">Milana Prokhorova</h3>
+        <h2 class="text-center text-xs">Founder / Director</h2>
+      </div>
+      <p class="text-center text-xs mt-10">
+        I believe people with passion can change the world and if you are one of
+        those believers then you are part of my tribe. Tredwear is born out of
+        my passion for shoes & a promise I made to myself, to create high
+        quality, true value shoes based on my 22+ years of shoe design knowledge
+        and experience working with multiple world famous brands.
+      </p>
+      <p class="text-center text-xs mt-1">
+        I grew up with next to nothing, all I had was love from my family and
+        friends, and a talent to create and sketch designs. I worked hard,
+        learnt and improved drastically and I soon found my way to the top. But
+        the top was not just enough, I wanted to mean more than just a sack of
+        profit to business organisations, I wanted my products to represent more
+        than profit and marginalisation. So, I decided to start all over again,
+        with the creation of Tredwear.
+      </p>
+      <p class="text-center text-xs mt-1">
+        Jazba stands for an undying passion to achieve and excel. The brand
+        symbolizes an attitude to do or die trying which has been my life's
+        motto. It represents the true spirit of sportsmanship, the urge to
+        perform in any field/ activity with undiluted passion. Its Focus is on
+        strength of character and attitude. Tredwear was created by me to
+        enhance belief in self and confidence. I want to see you excel without
+        losing yourself, Tredwear wants to be your backbone and your source of
+        inspiration, not just a brand, a movement, a movement of hardworking and
+        truthful people who have no limitations or restrictions.
+      </p>
+      <p class="text-center text-xs mt-6">
+        Come join this movement of freedom to succeed and I promise you Tredwear
+        will be there to support you all the way. Also feel free to share your
+        inputs or feedback with me on
+        <span class="text-accent-orange">tredwear.com</span> always, to help
+        make Tredwear better.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
